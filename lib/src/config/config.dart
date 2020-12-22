0c7059387cfd1543087289de07c72bd313df3248
Nightmare-MY <906262255@qq.com>
2020-12-22 21:06:42 +0800

    Update config.dart

--- lib/src/config/config.dart
+++ lib/src/config/config.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:file_manager/src/provider/file_manager_notifier.dart';
 import 'package:global_repository/global_repository.dart';
 
@@ -6,13 +8,20 @@ class Config {
   // 使用前一定先初始化
   static Future<void> initConfig() async {
     packageName ??= await PlatformUtil.packageName;
+    if (!Directory(frameworkPath).existsSync()) {
+      Directory(frameworkPath).create(
+        recursive: true,
+      );
+    }
   }
 
   static String packageName;
+  static String appName = 'FileManager';
   static const String backupPath = 'YanTool/Backup';
   static String binPath = '/data/data/$packageName/files/usr/bin';
   static String filesPath = '/data/data/$packageName/files';
-  static String appName = 'YanTool';
-  static String dataPath = '/data/data/$packageName';
+  static String dataPath = '$filesPath/$appName';
+  static String aaptPath = '$filesPath/$appName/aapt';
+  static String frameworkPath = dataPath + '/Framework';
   static FiMaPageNotifier fiMaPageNotifier;
 }
