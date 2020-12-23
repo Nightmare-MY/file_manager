7980c1255c792e12ee4c439a55e579bbc16faa28
Nightmare-MY <906262255@qq.com>
2020-12-23 17:08:52 +0800

    添加全局单例类

---
+++ lib/src/config/global.dart
@@ -0,0 +1,21 @@
+import 'package:event_bus/event_bus.dart';
+
+class Global {
+  // 工厂模式
+  factory Global() => _getInstance();
+  Global._internal() {
+    // TODO
+  }
+  // 用户信息
+  // 主题状态
+  static Global get instance => _getInstance();
+  static Global _instance;
+
+  static Global _getInstance() {
+    _instance ??= Global._internal();
+    return _instance;
+  }
+
+  EventBus eventBus = EventBus();
+  Future<void> initGlobal() async {}
+}
