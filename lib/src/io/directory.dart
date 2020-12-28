9b6ecc7b20ec7753068329678a8eb96937aeaed5
Nightmare-MY <906262255@qq.com>
2020-12-28 23:46:55 +0800

    Update directory.dart

--- lib/src/io/directory.dart
+++ lib/src/io/directory.dart
@@ -40,7 +40,7 @@ class NiDirectory extends FileEntity {
 
   NiDirectory get parent => NiDirectory(FileSystemEntity.parentOf(path));
   Future<List<FileEntity>> listAndSort({
-    bool verbose = true,
+    bool verbose = false,
   }) async {
     // if (Platform.isWindows) {
     //   return await listAndSortForWin();
@@ -59,7 +59,7 @@ class NiDirectory extends FileEntity {
     int _startIndex;
     List<String> _fullmessage = <String>[];
     path = path.replaceAll('//', '/');
-    print('刷新的路径=====>>${PlatformUtil.getUnixPath(path)}');
+    // print('刷新的路径=====>>${PlatformUtil.getUnixPath(path)}');
     final String lsOut = await NiProcess.exec(
       '$lsPath -aog "${PlatformUtil.getUnixPath(path)}"\n',
     );
